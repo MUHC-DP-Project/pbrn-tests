cfea2b81dfbb1cf36a0c502c45dfddf00d42af42
Max Brodeur-Urbas <murbas98@gmail.com>
2021-04-28 15:45:10 -0400

    project connection and deletion testing

---
+++ test/sample_project.js
@@ -0,0 +1,68 @@
+
+let sample_project1 = 
+    {
+    // participants
+    principalInvestigators: ["TEST_USER@mail.com"],
+    coInvestigators: [],
+    collaborators: [],
+    studySize:"Test",
+    studyParticipants:["Test","Test1","Test2"],
+
+    // Project
+    keywords:["Test","Test1","Test2"],
+    intendedMcgillLocation:[],
+    intendedNonMcgillLocation:[],
+    researchAndMethodology:[],
+    officialProjectTitle:"Test",
+    briefProjectTitle:"Test",
+    startDateProject: "Test",
+    endDateProject: "Test",
+    projectDescription: "Test",
+    projectFund:"Test",
+    projectSought:"Test",
+    agencyName:"Test",
+
+    scientificPeerReviewSelect:"Test",
+    scientificPeerReviewText:"Test",
+    studyIRBREBSelect:"Test",
+    studyIRBREBText:"Test",
+
+    // ResearchAndMethodology
+    intendedMcgillLocation: ["Test"],
+    intendedNonMcgillLocation: ["Test"],
+    researchAndMethodology: ["Test"],
+    projectConception: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    projectDesigned: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    fundingSoughtIgnoredConsidered: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    ethicsApproval: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    recruitment: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    dataCollection: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    dataAnalysis: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    },
+    knowledgeTranslationDissemination: {
+        estimatedDate: "2021-04-19T16:39:00.000Z",
+        projectStage: "In-progress"
+    }
+    };
+
+module.exports = {sample_project1};
--- test/systemTesting.js
+++ test/systemTesting.js
@@ -1,15 +1,23 @@
 const { assert, expect } = require("chai");
 const chai = require("chai");
-const mongoose = require('mongoose');
-const connection = require('mongoose').connection;
 const { exit } = require('process');
 require('dotenv').config();
+const mongoose = require('mongoose');
 let projectBaseUrl = "http://localhost:8080/projects/";
 let userBaseUrl = "http://localhost:8081/users/";
+let authBaseUrl = "http://localhost:8081/auth/";
+const {sample_user} = require("./sample_user");
+const {sample_user_connection} = require("./sample_user_connection");
+const {sample_project1} = require("./sample_project");
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 
-let sampleId = 1;
+const godToken =  process.env.GOD_TOKEN;
+
+// Database connector
+const clearDB = () => {
+    mongoose.connection.db.dropDatabase(() => {});
+  }
 
 before(async() => {
     const MONGO_DB_URI = process.env.MONGO_TEST_DB_URI;
@@ -28,18 +36,93 @@ after(async() => {
 });
 
 afterEach(async() => {
-    const collections = Object.keys(mongoose.connection.collections);
-
-    for (const collectionName of collections) {
-        const collection = mongoose.connection.collections[collectionName];
-        await collection.deleteMany({});
-    }
-})
-
-describe('App',function(){
-    it('first test', function(){
-        assert.equal(true, true);
-    });
+    await clearDB();
 });
 
+describe("User project connection tests", () => {
 
+    it('it connect a user to a project by the correct relationship', async() => {
+        //CREATING USER
+        return chai.request(authBaseUrl)
+        .post("signUp")
+        .send(sample_user)
+        .then((user) => {
+            //CREATING PROJECT WITH USER
+            return chai.request(projectBaseUrl)
+            .post("")
+            .send(sample_project1)
+            .set("Authorization", "Bearer " + godToken)
+            .then((project) => {
+                //LINKING USERS TO PROJECT
+                return chai.request(userBaseUrl)
+                .put("/connectToProjects/" + project.body._id)
+                .send(sample_user_connection)
+                .set("Authorization", "Bearer " + godToken)
+                .then((linkedUserRes) => {
+                    return chai.request(userBaseUrl)
+                    .get(user.body._id)
+                    .set("Authorization", "Bearer " + godToken)
+                    .then((updatedUser) => {
+                        console.log(updatedUser.body);
+                        expect(updatedUser).to.have.status(200);
+                        let projectList = updatedUser.body.PIListOfProjects;
+                        expect(projectList).to.have.length(1);
+                })
+            })
+        }).catch( (err) => {
+            console.log(err);
+            assert.fail();
+        })
+        })
+    });
+
+
+    it('it should delete the project ID in each involved user', async() => {
+        //CREATING USER
+        return chai.request(authBaseUrl)
+        .post("signUp")
+        .send(sample_user)
+        .then((user) => {
+            //CREATING PROJECT WITH USER
+            return chai.request(projectBaseUrl)
+            .post("")
+            .send(sample_project1)
+            .set("Authorization", "Bearer " + godToken)
+            .then((project) => {
+                //LINKING USERS TO PROJECT
+                return chai.request(userBaseUrl)
+                .put("/connectToProjects/" + project.body._id)
+                .send()
+                .set("Authorization", "Bearer " + godToken)
+                .then((linkedUserRes) => {
+                    //DELETING PROJECT
+                    return chai.request(projectBaseUrl)
+                    .delete(project.body._id.toString())
+                    .set("Authorization", "Bearer " + godToken)
+                    .then((deletionRes) => {
+                        //DELETE CONNECTION IN USERS
+                        return chai.request(userBaseUrl)
+                        .post("removeProjectConnection")
+                        .set("Authorization", "Bearer " + godToken)
+                        .send(deletionRes.body)
+                        .then((connectionDeletionRes) => {
+                            //GET UPDATED USER
+                            return chai.request(userBaseUrl)
+                            .get(user.body._id)
+                            .set("Authorization", "Bearer " + godToken)
+                            .then((updatedUser) => {
+                                //CHECK FOR DELETION WORKED
+                                expect(updatedUser).to.have.status(200);
+                                projectList = updatedUser.body.PIListOfProjects;
+                                expect(projectList).to.have.length(0);
+                            })
+                        })
+                    })
+                })
+            })
+        }).catch( (err) => {
+            console.log(err);
+            assert.fail();
+        })
+    })
+});
